dcd4e513b543aa77449dc82dd50b7ad566e5eff0
Dominik Zawidzki <dominik@Air-Dominik.Dlink>
2023-08-26 12:42:31 +0200

    fix: fixed email template sender name not showing up

--- src/assets/components/Contact.jsx
+++ src/assets/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
                 <h1>Get in touch with me!</h1>
                 <form ref={form} onSubmit={sendEmail} className='contact-form'>
                     <label>Name:</label>
-                    <input type="text" name="user_name" placeholder='Your Name'/>
+                    <input type="text" name="from_name" placeholder='Your Name'/>
                     <label>Email:</label>
                     <input type="email" name="user_email" placeholder='Your Email'/>
                     <label>Message:</label>
